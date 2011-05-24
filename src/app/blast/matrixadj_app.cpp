557832cefe33976160cdc7fbd08a8df16156b715
mkuhn <mkuhn@localhost>
2011-05-24 10:15:15 +0200

    first success in printing a matrix: iterate through query and subject sequences, print matrices. still need to specify lambda manually

--- src/app/blast/matrixadj_app.cpp
+++ src/app/blast/matrixadj_app.cpp
@@ -46,6 +46,11 @@ static char const rcsid[] =
 #include <algo/blast/format/blast_format.hpp>
 #include "blast_app_util.hpp"
 
+#include <algo/blast/composition_adjustment/composition_adjustment.h>
+#include <algo/blast/composition_adjustment/composition_constants.h>
+#include "../../algo/blast/core/matrix_freq_ratios.h"
+#include "../../algo/blast/api/blast_setup.hpp"
+
 #ifndef SKIP_DOXYGEN_PROCESSING
 USING_NCBI_SCOPE;
 USING_SCOPE(blast);
@@ -82,6 +87,54 @@ void CBlastpApp::Init()
     HideStdArgs(fHideLogfile | fHideConffile | fHideFullVersion | fHideXmlHelp | fHideDryRun);
     SetupArgDescriptions(m_CmdLineArgs->SetCommandLine());
 }
+
+/**
+ * A callback routine: compute lambda for the given score
+ * probabilities.
+ * (@sa calc_lambda_type).
+ */
+static double
+s_CalcLambda(double probs[], int min_score, int max_score, double lambda0)
+{
+   
+    int i;                 /* loop index */      
+    int score_range;       /* range of possible scores */
+    double avg;            /* expected score of aligning two characters */
+    Blast_ScoreFreq freq;  /* score frequency data */
+
+    score_range = max_score - min_score + 1;
+    avg = 0.0;
+    for (i = 0;  i < score_range;  i++) {
+        avg += (min_score + i) * probs[i];
+    }
+    freq.score_min = min_score;
+    freq.score_max = max_score;
+    freq.obs_min = min_score;
+    freq.obs_max = max_score;
+    freq.sprob0 = probs;
+    freq.sprob = &probs[-min_score];
+    freq.score_avg = avg;
+
+    return Blast_KarlinLambdaNR(&freq, lambda0);
+}
+
+
+void PrintMatrix(Int4 **matrix, ostream& out)
+{
+    for (int i = 0; i < BLASTAA_SIZE; i++) out << "\t" << NCBISTDAA_TO_AMINOACID[i];
+    out << "\n";
+
+    for (int i = 0; i < BLASTAA_SIZE; i++)
+    {
+        out << NCBISTDAA_TO_AMINOACID[i];
+        for (int j = 0; j < BLASTAA_SIZE; j++)
+        {
+            out  << "\t" << matrix[i][j];
+        }
+        out << "\n";
+    }
+}
+
 
 int CBlastpApp::Run(void)
 {
@@ -120,60 +173,113 @@ int CBlastpApp::Run(void)
                          db_adapter, scope);
         _ASSERT(db_adapter && scope);
 
-        /*** Get the formatting options ***/
-        CRef<CFormattingArgs> fmt_args(m_CmdLineArgs->GetFormattingArgs());
-        CBlastFormat formatter(opt, *db_adapter,
-                               fmt_args->GetFormattedOutputChoice(),
-                               query_opts->GetParseDeflines(),
-                               m_CmdLineArgs->GetOutputStream(),
-                               fmt_args->GetNumDescriptions(),
-                               fmt_args->GetNumAlignments(),
-                               *scope,
-                               opt.GetMatrixName(),
-                               fmt_args->ShowGis(),
-                               fmt_args->DisplayHtmlOutput(),
-                               opt.GetQueryGeneticCode(),
-                               opt.GetDbGeneticCode(),
-                               opt.GetSumStatisticsMode(),
-                               m_CmdLineArgs->ExecuteRemotely(),
-                               db_adapter->GetFilteringAlgorithm(),
-                               fmt_args->GetCustomOutputFormatSpec());
+        // Get a IQueryFactory from our CLocalDbAdapter, using a custom method added to this class
+        CRef<IQueryFactory> subjects = db_adapter->GetSubjectFactory();
+        CRef<ILocalQueryData> local_subject_data = subjects->MakeLocalQueryData(&opt);
+        BLAST_SequenceBlk* subject = local_subject_data->GetSequenceBlk();
         
-        formatter.PrintProlog();
+        // Setup for composition-based adjustment
+        Blast_CompositionWorkspace *NRrecord = Blast_CompositionWorkspaceNew();
+        int status_code = Blast_CompositionWorkspaceInit(NRrecord, opt.GetMatrixName());
 
+        _ASSERT(status_code == 0);
+        
+        static const int kReMatrixAdjustmentPseudocounts = 20;
+        static const int compositionTestIndex = 0;
+        
         /*** Process the input ***/
-        for (; !input.End(); formatter.ResetScopeHistory()) {
+        for (; !input.End(); ) {
 
+            // Get queries
             CRef<CBlastQueryVector> query_batch(input.GetNextSeqBatch(*scope));
             CRef<IQueryFactory> queries(new CObjMgr_QueryFactory(*query_batch));
 
-            SaveSearchStrategy(args, m_CmdLineArgs, queries, opts_hndl);
+            CRef<ILocalQueryData> local_query_data = queries->MakeLocalQueryData(&opt);
+            
+            BLAST_SequenceBlk* query = local_query_data->GetSequenceBlk();
 
-            CRef<CSearchResultSet> results;
+            // Loop through all combinations of subjects and queries
+            for (size_t query_index=0; query_index < local_query_data->GetNumQueries(); query_index++)
+            {
+                CConstRef<CSeq_loc> qseqloc = local_query_data->GetSeq_loc(query_index);
+                
+                // keep track of position within the subject BLAST_SequenceBlk
+                size_t current_start = 0;
+                size_t current_end = 0;
+                
+                // Determine query composition
+                Blast_AminoAcidComposition query_composition;
+                Blast_ReadAaComposition(&query_composition, BLASTAA_SIZE, query->sequence, query->length);
+                
+                for (size_t subject_index=0; subject_index < local_subject_data->GetNumQueries(); subject_index++)
+                {
+                     CConstRef<CSeq_loc> sseqloc = local_subject_data->GetSeq_loc(subject_index);
+                     int subject_length = sseqloc->GetStop(eExtreme_Positional);
+                     
+                     current_start = current_end;
+                     current_end = current_start + subject_length;
+                     
+                     printf("Start: %zu\n", current_start);
+                     printf("End: %zu\n", current_end);
 
-            if (m_CmdLineArgs->ExecuteRemotely()) {
-                CRef<CRemoteBlast> rmt_blast = 
-                    InitializeRemoteBlast(queries, db_args, opts_hndl,
-                          m_CmdLineArgs->ProduceDebugRemoteOutput(),
-                          m_CmdLineArgs->GetClientId());
-                results = rmt_blast->GetResultSet();
-            } else {
-                CLocalBlast lcl_blast(queries, opts_hndl, db_adapter);
-                lcl_blast.SetNumberOfThreads(m_CmdLineArgs->GetNumThreads());
-                results = lcl_blast.Run();
-            }
+                     // Determine subject composition
+                     Blast_AminoAcidComposition subject_composition;
+                     Blast_ReadAaComposition(&subject_composition, BLASTAA_SIZE, &subject->sequence[current_start], subject_length);
+                     
+                     Blast_MatrixInfo *scaledMatrixInfo = Blast_MatrixInfoNew(BLASTAA_SIZE, BLASTAA_SIZE, 0);
+                     scaledMatrixInfo->matrixName = strdup(opt.GetMatrixName());
+                     scaledMatrixInfo->ungappedLambda = 0.31; // FIXME
+                    
+                     /* Frequency ratios for the matrix */
+                     SFreqRatios * stdFreqRatios = NULL;
 
-            if (fmt_args->ArchiveFormatRequested(args)) {
-                formatter.WriteArchive(*queries, *opts_hndl, *results);
-            } else {
-                BlastFormatter_PreFetchSequenceData(*results, scope);
-                ITERATE(CSearchResultSet, result, *results) {
-                    formatter.PrintOneResultSet(**result, query_batch);
+                     stdFreqRatios = _PSIMatrixFrequencyRatiosNew(scaledMatrixInfo->matrixName);
+                     for (int i = 0;  i < BLASTAA_SIZE;  i++) {
+                         for (int j = 0;  j < BLASTAA_SIZE;  j++) {
+                             scaledMatrixInfo->startFreqRatios[i][j] = stdFreqRatios->data[i][j];
+                         }
+                     }
+                     stdFreqRatios = _PSIMatrixFrequencyRatiosFree(stdFreqRatios);
+
+                     Blast_Int4MatrixFromFreq(scaledMatrixInfo->startMatrix, scaledMatrixInfo->cols,
+                                              scaledMatrixInfo->startFreqRatios, scaledMatrixInfo->ungappedLambda);
+                    
+
+                     /* which mode of composition adjustment is actually used? */
+                     EMatrixAdjustRule matrix_adjust_rule = eDontAdjustMatrix;
+                     double pvalueForThisPair = (-1); /* p-value for this match for composition; -1 == no adjustment*/
+                     double LambdaRatio; /*lambda ratio*/
+
+                     BlastScoreBlk *sbp = BlastScoreBlkNew(BLASTAA_SEQ_CODE, 1);
+                     sbp->name = strdup(opt.GetMatrixName());
+                     Blast_ScoreBlkMatrixFill(sbp, &BlastFindMatrixPath);
+                     
+                     PrintMatrix(sbp->matrix->data, cerr);
+                     
+                     int adjust_search_failed =
+                         Blast_AdjustScores(sbp->matrix->data,
+                                            &query_composition, query->length,
+                                            &subject_composition, subject_length,
+                                            scaledMatrixInfo, eCompositionBasedStats,
+                                            kReMatrixAdjustmentPseudocounts, NRrecord,
+                                            &matrix_adjust_rule, &s_CalcLambda,
+                                            &pvalueForThisPair,
+                                            compositionTestIndex,
+                                            &LambdaRatio);
+
+                    PrintMatrix(sbp->matrix->data, cerr);
+
+                    _ASSERT(adjust_search_failed == 0);
+                    
+                     current_end++;
                 }
             }
-        }
+            
 
-        formatter.PrintEpilog(opt);
+            Blast_CompositionWorkspaceFree(&NRrecord);
+
+
+        }
 
         if (m_CmdLineArgs->ProduceDebugOutput()) {
             opts_hndl->GetOptions().DebugDumpText(NcbiCerr, "BLAST options", 1);
